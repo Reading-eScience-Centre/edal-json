951ab2e15b73998f2f5724698c9c3eedb9892f0c
Maik Riechert <maik.riechert@arcor.de>
2015-06-11 14:15:11 +0100

    expose profiles and rectilinear grids

--- src/main/java/uk/ac/rdg/resc/edal/json/FeatureResource.java
+++ src/main/java/uk/ac/rdg/resc/edal/json/FeatureResource.java
@@ -8,6 +8,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import org.joda.time.DateTime;
 import org.opengis.metadata.extent.GeographicBoundingBox;
 import org.restlet.data.Reference;
 import org.restlet.ext.json.JsonRepresentation;
@@ -21,11 +22,15 @@ import uk.ac.rdg.resc.edal.domain.GridDomain;
 import uk.ac.rdg.resc.edal.exceptions.EdalException;
 import uk.ac.rdg.resc.edal.feature.DiscreteFeature;
 import uk.ac.rdg.resc.edal.feature.Feature;
+import uk.ac.rdg.resc.edal.feature.GridFeature;
+import uk.ac.rdg.resc.edal.feature.ProfileFeature;
 import uk.ac.rdg.resc.edal.geometry.BoundingBox;
+import uk.ac.rdg.resc.edal.grid.RectilinearGrid;
 import uk.ac.rdg.resc.edal.grid.RegularGrid;
 import uk.ac.rdg.resc.edal.grid.TimeAxis;
 import uk.ac.rdg.resc.edal.grid.VerticalAxis;
 import uk.ac.rdg.resc.edal.metadata.Parameter;
+import uk.ac.rdg.resc.edal.position.HorizontalPosition;
 import uk.ac.rdg.resc.edal.util.Array;
 import uk.ac.rdg.resc.edal.util.Array4D;
 
@@ -75,56 +80,101 @@ public class FeatureResource extends ServerResource {
 	}
 	
 	private Map getDomainJson(Feature<?> feature) {
-		Domain<?> domain = feature.getDomain();
 		Map domainJson;
 		
-		if (domain instanceof GridDomain) {
-			GridDomain grid = (GridDomain) domain;
-			if (grid.getHorizontalGrid() instanceof RegularGrid) {
-				RegularGrid reggrid = (RegularGrid) grid.getHorizontalGrid();
+		// FIXME feature types should be interfaces
+		if (feature instanceof GridFeature) {
+			GridFeature gridFeature = (GridFeature) feature;
+			GridDomain grid = gridFeature.getDomain();
+			if (grid.getHorizontalGrid() instanceof RectilinearGrid) {
+				RectilinearGrid rectgrid = (RectilinearGrid) grid.getHorizontalGrid();
 				TimeAxis t = grid.getTimeAxis();
 				VerticalAxis z = grid.getVerticalAxis();
+				// TODO vertical CRS?
 				
-				BoundingBox bb = reggrid.getBoundingBox();
+				BoundingBox bb = rectgrid.getBoundingBox();
 				domainJson = new HashMap(ImmutableMap.of(
-						"type", "RegularGrid",
-					    "crs", Utils.getCrsUri(reggrid.getCoordinateReferenceSystem()),
+						"type", "RectilinearGrid",
+					    "crs", Utils.getCrsUri(rectgrid.getCoordinateReferenceSystem()),
 					    "bbox", ImmutableList.of(bb.getMinX(), bb.getMinY(), bb.getMaxX(), bb.getMaxY()),
-					    "delta", ImmutableList.of(
-					    		reggrid.getXAxis().getCoordinateSpacing(),
-					    		reggrid.getYAxis().getCoordinateSpacing()
-					    		)
+					    "x", rectgrid.getXAxis().getCoordinateValues(),
+						"y", rectgrid.getYAxis().getCoordinateValues()
 						));
-				domainJson.putAll(ImmutableMap.of(
-						"x", reggrid.getXAxis().getCoordinateValues(),
-						"y", reggrid.getYAxis().getCoordinateValues()
-						));
-				
-				// TODO should we name the type "RegularGrid" even if z is irregular?
-				if (z != null) {
-					domainJson.put("vertical", z.getCoordinateValues());
-					domainJson.put("verticalUom", z.getVerticalCrs().getUnits());
-					domainJson.put("verticalPositiveUpwards", z.getVerticalCrs().isPositiveUpwards());
-				}
-				if (t == null) {
-					// TODO why is time null? shouldn't there be always a time?
-				} else if (t.size() > 1) {
+				if (rectgrid instanceof RegularGrid) {
+					RegularGrid reggrid = (RegularGrid) rectgrid;
 					domainJson.putAll(ImmutableMap.of(
-							"type", "RegularGridSeries",
-							"time", t.getCoordinateValues()
-							));
-				} else {
-					domainJson.put("time", t.getCoordinateValues().get(0));
+							"type", "RegularGrid",
+							"delta", ImmutableList.of(
+						    		reggrid.getXAxis().getCoordinateSpacing(),
+						    		reggrid.getYAxis().getCoordinateSpacing()
+						    		)						    
+						    ));
 				}
+				
+				// TODO should we name the type "RegularGrid" even if z or t is irregular?
+				addVerticalAxis(z, domainJson);
+				addTimeAxis(t, domainJson);
+
 			} else {
-				domainJson = unsupportedDomain(domain, grid.getHorizontalGrid().getClass().getName());
+				domainJson = unsupportedDomain(feature.getDomain(), grid.getHorizontalGrid().getClass().getName());
 			}
+		} else if (feature instanceof ProfileFeature) {
+			ProfileFeature profile = (ProfileFeature) feature;
+			VerticalAxis z = profile.getDomain();
+			DateTime t = profile.getTime();
+			HorizontalPosition pos = profile.getHorizontalPosition();
+			
+			domainJson = new HashMap(ImmutableMap.of(
+					"type", "Profile",
+				    "crs", Utils.getCrsUri(pos.getCoordinateReferenceSystem()),
+				    "bbox", ImmutableList.of(pos.getX(), pos.getY(), pos.getX(), pos.getY()),
+				    "x", pos.getX(),
+				    "y", pos.getY()
+					));
+			
+			addVerticalAxis(z, domainJson);
+			addTime(t, domainJson);
+			
 		} else {
-			domainJson = unsupportedDomain(domain);
+			// TODO should probably say unsupported feature
+			domainJson = unsupportedDomain(feature.getDomain());
 		}
 		
 		 
 		return domainJson;
+	}
+	
+	private void addVerticalAxis(VerticalAxis z, Map domainJson) {
+		if (z != null) {
+			domainJson.put("vertical", z.getCoordinateValues());
+			// TODO are there no standards for vertical CRS, with codes etc.?
+			domainJson.put("verticalCrs", ImmutableMap.of(
+					"uom", z.getVerticalCrs().getUnits(),
+					"positiveUpwards", z.getVerticalCrs().isPositiveUpwards(),
+					"dimensionless", z.getVerticalCrs().isDimensionless(),
+					"pressure", z.getVerticalCrs().isPressure()
+					));
+		}		
+	}
+		
+	private void addTimeAxis(TimeAxis t, Map domainJson) {
+		if (t == null) {
+			// TODO why is time null? shouldn't there be always a time?
+		} else if (t.size() > 1) {
+			domainJson.putAll(ImmutableMap.of(
+					"time", t.getCoordinateValues()
+					));
+		} else {
+			domainJson.put("time", t.getCoordinateValues().get(0));
+		}
+	}
+	
+	private void addTime(DateTime t, Map domainJson) {
+		// TODO profile should have a timeaxis with a single element
+		// this would avoid special handling here
+		if (t != null) {
+			domainJson.put("time", t.toString());
+		}
 	}
 	
 	private static Map unsupportedDomain(Domain<?> domain) {
