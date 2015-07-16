7dd2211ba77f2460a2a2f9ca420c45ceadb392a8
Maik Riechert <maik.riechert@arcor.de>
2015-07-16 16:45:33 +0100

    try to include jQRangeSlider via override, still broken

--- html/src/app/controls.js
+++ html/src/app/controls.js
@@ -1,10 +1,11 @@
 import $ from 'jquery'
 import L from 'leaflet'
-import 'jquery-ui'
-import 'jquery-mousewheel'
+//import 'jquery-ui'
+//import 'jquery-mousewheel'
 // jqrangeslider not yet on JSPM registry
-import 'app/jqrangeslider/jQAllRangeSliders-min'
-import 'app/jqrangeslider/iThing-min.css!'
+//import 'app/jqrangeslider/jQAllRangeSliders-min'
+//import 'app/jqrangeslider/iThing-min.css!'
+import 'jQRangeSlider'
 import * as utils from 'app/utils'
 
 function redrawLayers (map, tilesOnly=false) {
