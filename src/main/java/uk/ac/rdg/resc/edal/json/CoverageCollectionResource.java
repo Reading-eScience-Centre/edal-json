c69ce49c9e7fd1b3a69f5e8ffa9bda8cf3ac80bb
Maik Riechert <maik.riechert@arcor.de>
2016-02-06 13:40:20 +0000

    write collection profile for collections of single domain type
    e.g. VerticalProfileCoverageCollection

--- src/main/java/uk/ac/rdg/resc/edal/json/CoverageCollectionResource.java
+++ src/main/java/uk/ac/rdg/resc/edal/json/CoverageCollectionResource.java
@@ -303,6 +303,7 @@ public class CoverageCollectionResource extends ServerResource {
 			if (datasetMeta.getFeatureTypes().size() == 1) {
 				String firstFeatureId = datasetMeta.getFeatureIds().iterator().next();
 				String domainType = datasetMeta.getFeatureMetadata(firstFeatureId).domainMeta.getType();
+				j.put("profile", domainType + "CoverageCollection");
 				// TODO implement
 				/*
 				j.put("domainTemplate", ImmutableMap.of(
